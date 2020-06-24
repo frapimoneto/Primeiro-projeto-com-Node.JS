1cd4be832f39da520f472ecb947c20853037f60e
Francisco Pinto Moreira Neto <31639140+frapimoneto@users.noreply.github.com>
2020-06-24 09:47:03 -0300

    debug

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,20 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello GoStack!' })
+})
+
+app.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email
+  }
+  return response.json(user);
 })
 
 app.listen(3333, () => {
