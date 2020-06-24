acce9cf2981b97b291e1205e2f0a056f480dbda3
Francisco Pinto Moreira Neto <31639140+frapimoneto@users.noreply.github.com>
2020-06-24 13:23:00 -0300

    appointments

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoStack!' })
-})
-
-app.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email
-  }
-  return response.json(user);
-})
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!')
